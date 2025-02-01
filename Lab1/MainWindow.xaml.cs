c29a00a30b9c2bba6d0ba10362134a92e4d70426
Maksim Dadush <pud700max@gmail.com>
2025-02-01 22:26:06 +0300

    Add project files.

---
+++ Lab1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly OpenFileDialog ofd;
+
+        public MainWindow() {
+            InitializeComponent();
+
+            ofd = new OpenFileDialog {
+                 CheckFileExists = true,
+                 CheckPathExists = true,
+                 Multiselect = false,
+                 Filter = "OBJ files (*.obj)|*.obj"
+             };
+            ofd.FileOk += OnFileOpened;
+
+            Height = SystemParameters.PrimaryScreenHeight / 1.25;
+            Width = SystemParameters.PrimaryScreenWidth / 1.25;
+        }
+
+        private void OnFileOpened(object? sender, CancelEventArgs e) {
+            fileName.Text = string.Join(' ', Resources["fileString"].ToString(), ofd.FileName);
+        }
+
+        private void ButtonOpenFile_Click(object sender, RoutedEventArgs e) {
+            ofd.ShowDialog();
+        }
+
+
+    }
+}
