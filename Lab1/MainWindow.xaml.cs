625a65589443e1bc10624c25eb0f7497e3d8efca
Maksim Dadush <pud700max@gmail.com>
2025-02-03 21:54:46 +0300

    UI + SetPixel for .NET

--- Lab1/MainWindow.xaml.cs
+++ Lab1/MainWindow.xaml.cs
@@ -36,6 +36,14 @@ namespace Lab1
 
         private void OnFileOpened(object? sender, CancelEventArgs e) {
             fileName.Text = string.Join(' ', Resources["fileString"].ToString(), ofd.FileName);
+
+            WriteableBitmap bitmap = new WriteableBitmap(
+                ((int)canvas.ActualWidth), ((int)canvas.ActualHeight), 96, 96, PixelFormats.Bgra32, null);
+
+            bitmap.SetPixel(100, 100, 0xFFFFFFFF);
+            bitmap.SetPixel(101, 101, 0xFFFFFFFF);
+
+            canvas.Child = new Image { Source = bitmap };
         }
 
         private void ButtonOpenFile_Click(object sender, RoutedEventArgs e) {
---
+++ Lab1/PixelExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using System.Windows.Media;
+using System.Runtime.CompilerServices;
+
+namespace Lab1 {
+    public static class PixelExtensions {
+
+        private static Int32Rect defaultRect = new Int32Rect(0, 0, 1, 1);
+
+        public static void BeginLock(this WriteableBitmap bitmap) {
+            bitmap.Lock();
+        }
+
+        public static unsafe void SetPixelLocked(this WriteableBitmap bitmap, int x, int y, uint argb) {
+            unsafe {
+                uint* ptr = (uint*)bitmap.BackBuffer;
+                ptr[y * 4 + x] = argb;
+            }
+            defaultRect.X = x;
+            defaultRect.Y = y;
+            bitmap.AddDirtyRect(defaultRect);
+        }
+        
+        public static void EndLock(this WriteableBitmap bitmap) {
+            bitmap.Unlock();
+        }
+
+        public static void SetPixel(this WriteableBitmap bitmap, int x, int y, uint color) {
+            defaultRect.X = x;
+            defaultRect.Y = y;
+            bitmap.WritePixels(defaultRect, BitConverter.GetBytes(color), 4, 0);
+        }
+    }
+}
