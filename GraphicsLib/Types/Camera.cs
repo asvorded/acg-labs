112cb707265f3bf84043d95d54fd68e5481b781e
артур левицкий <levitskijartur@gmail.com>
2025-02-04 16:41:05 +0300

    abc

--- GraphicsLib/Types/Camera.cs
+++ GraphicsLib/Types/Camera.cs
@@ -9,7 +9,26 @@ namespace GraphicsLib.Types
 {
     public class Camera
     {
-        public Vector3 Position { get; set; }
-        public Quaternion Rotation { get; set; }
+        private Vector3 _position;
+        private Vector3 _lookAtPos;
+        private Matrix4x4 _viewMatrix;
+
+        public Vector3 Position { get => _position; set => SetPosition(value); }
+        public Vector3 LookAtPos { get => _lookAtPos; set => _lookAtPos = value; }
+        public Matrix4x4 ViewMatrix { get => _viewMatrix; private set => _viewMatrix = value; }
+
+        public Camera(Vector3 position, Vector3 lookAtPos)
+        {
+            Position = position;
+            LookAtPos = lookAtPos;
+        }
+        private void SetPosition(Vector3 newPos)
+        {
+            _position = newPos;
+        }
+        private void UpdateViewMatrix()
+        {
+            _viewMatrix = Matrix4x4.CreateLookAt(_position,_lookAtPos,)
+        }
     }
 }
