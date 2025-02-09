98ea9a295713c779e0f0a57b8245cd053e2682d6
Artur Levitskij <levitskijartur@gmail.com>
2025-02-09 23:28:06 +0300

    parser v0.1

--- GraphicsLib/Parser.cs
+++ GraphicsLib/Parser.cs
@@ -7,8 +7,19 @@ using System.Numerics;
 using System.Text;
 namespace GraphicsLib
 {
-    public class Parser
+    using GL_SCALAR = Int16;
+    public static class Parser
     {
+
+        static readonly int GL_ARRAY_BUFFER = 34962;
+        static readonly int GL_ELEMENT_ARRAY_BUFFER = 34963;
+        static readonly int GL_TRIANGLES = 4;
+        static readonly int GL_TRIANGLE_STRIP = 5;
+        static readonly int GL_TRIANGLE_FAN = 6;
+        static readonly string GL_VEC3_TYPE = "VEC3";
+        static readonly string GL_SCALAR_TYPE = "SCALAR";
+        static readonly int GL_VEC3_SIZE = sizeof(Single) * 3;
+        static readonly int GL_SCALAR_SIZE = sizeof(GL_SCALAR);
         /// <summary>
         /// Загружает модель из текста формата Wavefront Obj
         /// </summary>
@@ -128,19 +139,161 @@ namespace GraphicsLib
         }
         public static Obj ParseGltfFile(Stream source, String sourceFileDirectory)
         {
-                Obj obj = new();
-                using StreamReader sr = new(source);
-                String data = sr.ReadToEnd();
-                dynamic gltfData = JsonConvert.DeserializeObject<dynamic>(data);
-                List<byte[]> buffers = [];
-                if (gltfData == null)
-                    throw new FormatException("invalid json file");
-                foreach (var buffer in gltfData.buffers)
+            Obj obj = new();
+            using StreamReader sr = new(source);
+            string data = sr.ReadToEnd();
+            dynamic gltfData = JsonConvert.DeserializeObject<dynamic>(data)
+                ?? throw new FormatException("invalid json gltf");
+            List<byte[]> buffers = [];
+            List<dynamic> bufferViews = [];
+            List<dynamic> accessors = [];
+            foreach (var buffer in gltfData.buffers)
+            {
+                String uri = buffer.uri;
+                buffers.Add(GetBufferData(uri, sourceFileDirectory));
+            }
+            foreach (var bufferView in gltfData.bufferViews)
+            {
+                bufferViews.Add(new
                 {
-                    String uri = buffer.uri;
-                    buffers.Add(GetBufferData(uri, sourceFileDirectory));
+                    buffer = buffers[(int)bufferView.buffer],
+                    bufferView.byteLength,
+                    bufferView.byteOffset,
+                    bufferView.target
+                });
+            }
+            foreach (var accessor in gltfData.accessors)
+            {
+                accessors.Add(new
+                {
+                    bufferView = bufferViews[(int)accessor.bufferView],
+                    accessor.componentType,
+                    accessor.count,
+                    accessor.type,
+                    accessor.byteOffset
+                });
+            }
+            foreach (var mesh in gltfData.meshes)
+            {
+                foreach (var primitive in mesh.primitives)
+                {
+                    int vIndexOffset = obj.vertices.Count;
+                    if (primitive.attributes.POSITION != null)
+                    {
+                        var accessor = accessors[(int)primitive.attributes.POSITION];
+                        AddVectors(obj.vertices, accessor);
+                    }
+                    if (primitive.attributes.NORMAL != null)
+                    {
+                        var accessor = accessors[(int)primitive.attributes.NORMAL];
+                        AddVectors(obj.normals, accessor);
+                    }
+                    int mode = primitive.mode ?? GL_TRIANGLES;
+                    if (mode == GL_TRIANGLES || mode == GL_TRIANGLE_STRIP || mode == GL_TRIANGLE_FAN)
+                    {
+                        var accessor = accessors[(int)primitive.indices];
+                        AddFaces(obj.faces, accessor, mode, vIndexOffset);
+                        continue;
+                    }
+                    throw new NotImplementedException($"wtf is this primitive: {mode}");
                 }
-                return obj;
+            }
+            return obj;
+        }
+        private static GL_SCALAR ParseGlScalarFromBytes(byte[] bytes, int offset)
+        {
+            return BitConverter.ToInt16(bytes, offset);
+        }
+        private static void AddFaces(List<Face> faces, dynamic accessor, int mode, int vIndexOffset)
+        {
+            string type = accessor.type;
+            if (!type.Equals("SCALAR"))
+            {
+                throw new Exception("accessor type missmatch");
+            }
+            int offset = accessor.byteOffset ?? 0;
+            int count = accessor.count;
+            var bufferView = accessor.bufferView;
+            int target = bufferView.target;
+            if (!target.Equals(GL_ELEMENT_ARRAY_BUFFER))
+            {
+                throw new Exception("bufferView target missmatch");
+            }
+            int byteLength = bufferView.byteLength;
+            if (!(byteLength >= offset + GL_SCALAR_SIZE * count))
+            {
+                throw new Exception("bufferView is not long enough");
+            }
+            offset += (int)bufferView.byteOffset;
+            var buffer = bufferView.buffer;
+            if (mode == GL_TRIANGLES)
+            {
+                for (int i = 0; i < count; i += 3)
+                {
+                    int[] vIndices = [ ParseGlScalarFromBytes(buffer, offset) + vIndexOffset,
+                                       ParseGlScalarFromBytes(buffer, offset + 1 * GL_SCALAR_SIZE) + vIndexOffset,
+                                       ParseGlScalarFromBytes(buffer, offset + 2 * GL_SCALAR_SIZE) + vIndexOffset ];
+                    Face face = new(vIndices, null, null);
+                    faces.Add(face);
+                    offset += 3 * GL_SCALAR_SIZE;
+                }
+            }
+            else if (mode == GL_TRIANGLE_STRIP)//HACK {1 2 3}  drawing order is (2 1 3) to maintain proper winding
+            {
+                for (int i = 2; i < count; i++)
+                {
+                    int[] vIndices = [ ParseGlScalarFromBytes(buffer, offset) + vIndexOffset,
+                                       ParseGlScalarFromBytes(buffer, offset + 1 * GL_SCALAR_SIZE) + vIndexOffset,
+                                       ParseGlScalarFromBytes(buffer, offset + 2 * GL_SCALAR_SIZE) + vIndexOffset ];
+                    Face face = new(vIndices, null, null);
+                    faces.Add(face);
+                    offset += GL_SCALAR_SIZE;
+                }
+            }
+            else if (mode == GL_TRIANGLE_FAN)
+            {
+                GL_SCALAR startIndex = ParseGlScalarFromBytes(buffer, offset) + vIndexOffset;
+                for (int i = 2; i < count; i++)
+                {
+                    int[] vIndices = [ startIndex,
+                                       ParseGlScalarFromBytes(buffer, offset + 1 * GL_SCALAR_SIZE) + vIndexOffset,
+                                       ParseGlScalarFromBytes(buffer, offset + 2 * GL_SCALAR_SIZE) + vIndexOffset ];
+                    Face face = new(vIndices, null, null);
+                    faces.Add(face);
+                    offset += GL_SCALAR_SIZE;
+                }
+            }
+        }
+        private static void AddVectors(List<Vector3> vectors, dynamic accessor)
+        {
+            string type = accessor.type;
+            if (!type.Equals(GL_VEC3_TYPE))
+            {
+                throw new Exception("accessor type missmatch");
+            }
+            int offset = accessor.byteOffset ?? 0;
+            int count = accessor.count;
+            var bufferView = accessor.bufferView;
+            int target = bufferView.target;
+            if (!target.Equals(GL_ARRAY_BUFFER))
+            {
+                throw new Exception("bufferView target missmatch");
+            }
+            int byteLength = bufferView.byteLength;
+            if (!(byteLength >= offset + GL_SCALAR_SIZE * count))
+            {
+                throw new Exception("bufferView is not long enough");
+            }
+            offset += (int)bufferView.byteOffset;
+            var buffer = bufferView.buffer;
+            for (int i = 0; i < count; i++)
+            {
+                Vector3 v = new(BitConverter.ToSingle(buffer, offset),
+                                BitConverter.ToSingle(buffer, offset + sizeof(Single)),
+                                BitConverter.ToSingle(buffer, offset + 2 * sizeof(Single)));
+                vectors.Add(v);
+                offset += 3 * sizeof(Single);
+            }
         }
     }
 }
